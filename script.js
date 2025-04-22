72b29dba669cb35b7dfe909077967232b8f3e950
nayelivitoria <nayvitoria785@gmail.com>
2025-04-22 10:39:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// Função para adicionar produtos ao carrinho (simples exemplo)
+document.querySelectorAll('.buy-button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Produto adicionado ao carrinho!');
+    });
+});
